698491c56d9675cb1a63483f77eef88a972aba22
RealByter <justavishay@gmail.com>
2023-06-10 19:13:04 +0300

    feat: style the stay component

--- src/components/Stay.js
+++ src/components/Stay.js
@@ -1,7 +1,7 @@
 import styles from './Stay.module.css';
 
 const Stay = props => (
-	<div>
+	<div className={styles.container}>
 		<img className={styles.photo} src={props.photo} alt="Apartment" />
 		<div className={styles.info}>
 			{props.superHost && <p className={styles['super-host']}>SUPER HOST</p>}
@@ -13,7 +13,7 @@ const Stay = props => (
 					<title>star</title>
 					<path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
 				</svg>
-				<p>{props.rating}</p>
+				<p>{props.rating.toFixed(2)}</p>
 			</div>
 		</div>
 		<p className={styles.title}>{props.title}</p>
