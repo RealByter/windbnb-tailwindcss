ba350d8dbee3a7860a67c70447e3976f752eae42
RealByter <justavishay@gmail.com>
2023-06-11 17:08:28 +0300

    feat: convert the stays component to tailwindcss

--- src/components/Stays.js
+++ src/components/Stays.js
@@ -1,14 +1,13 @@
-import styles from './Stays.module.css';
 import stays from '../assets/stays.json';
 import Stay from './Stay';
 
 const Stays = () => (
 	<>
-		<div className={styles.general}>
-			<h2>Stays in Finland</h2>
-			<p>12+ stays</p>
+		<div className="flex items-center mt-[2.375rem] mb-6 md:mt-24">
+			<h2 className="text-[1.125rem] font-bold">Stays in Finland</h2>
+			<p className="text-sm font-medium ml-auto">12+ stays</p>
 		</div>
-		<div className={styles.stays}>
+		<div className="flex mx-auto flex-wrap justify-center gap-y-[calc(50px+1.5rem)] gap-x-8 mb-40">
 			{stays.map((stay, index) => (
 				<Stay key={index} {...stay} />
 			))}
